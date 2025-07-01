888203d98e81e27ab67259440d9f68c3d0ccba33
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-01 16:53:05 +0100

    Update index.ts

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import { sectionsOnlyAndCardConversion } from "./sectionsOnlyAndCardConversion";
+import { sectionsOnlyAndContactValueConversion } from "./sectionsOnlyAndContactValueConversion";
 
 import { SummaryDetailsTransformationMap } from "server/transforms/summaryDetails/types";
 export { SummaryDetailsTransformationMap };
@@ -9,12 +9,12 @@ export { SummaryDetailsTransformationMap };
  * [View the docs for summary-details-transformations an explanation of how this feature works](docs/runner/summary-details-transforms.md)
  */
 const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
-  "close-contact-form": sectionsOnlyAndCardConversion,
-  "close-contact-form-uat": sectionsOnlyAndCardConversion,
-  "close-contact-form-nl5": sectionsOnlyAndCardConversion,
-  "close-contact-form-hpt": sectionsOnlyAndCardConversion,
-  "close-contact-form-hpt-uat": sectionsOnlyAndCardConversion,
-  "close-contact-form-hpt-nl5": sectionsOnlyAndCardConversion,
+  "close-contact-form": sectionsOnlyAndContactValueConversion,
+  "close-contact-form-uat": sectionsOnlyAndContactValueConversion,
+  "close-contact-form-nl5": sectionsOnlyAndContactValueConversion,
+  "close-contact-form-hpt": sectionsOnlyAndContactValueConversion,
+  "close-contact-form-hpt-uat": sectionsOnlyAndContactValueConversion,
+  "close-contact-form-hpt-nl5": sectionsOnlyAndContactValueConversion,
 };
 
 module.exports = summaryDetailsTransformations;
