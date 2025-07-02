b91aa03ee7d9e02a76d5d756f9215168f678e4c2
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-02 13:40:07 +0100

    Update index.ts

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import { sectionsOnlyAndContactValueConversion } from "./sectionsOnlyAndContactValueConversion";
+import { runMultipleTransformations } from "./runMultipleTransformations";
 
 import { SummaryDetailsTransformationMap } from "server/transforms/summaryDetails/types";
 export { SummaryDetailsTransformationMap };
@@ -9,12 +9,12 @@ export { SummaryDetailsTransformationMap };
  * [View the docs for summary-details-transformations an explanation of how this feature works](docs/runner/summary-details-transforms.md)
  */
 const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
-  "close-contact-form": sectionsOnlyAndContactValueConversion,
-  "close-contact-form-uat": sectionsOnlyAndContactValueConversion,
-  "close-contact-form-nl5": sectionsOnlyAndContactValueConversion,
-  "close-contact-form-hpt": sectionsOnlyAndContactValueConversion,
-  "close-contact-form-hpt-uat": sectionsOnlyAndContactValueConversion,
-  "close-contact-form-hpt-nl5": sectionsOnlyAndContactValueConversion,
+  "close-contact-form": runMultipleTransformations,
+  "close-contact-form-uat": runMultipleTransformations,
+  "close-contact-form-nl5": runMultipleTransformations,
+  "close-contact-form-hpt": runMultipleTransformations,
+  "close-contact-form-hpt-uat": runMultipleTransformations,
+  "close-contact-form-hpt-nl5": runMultipleTransformations,
 };
 
 module.exports = summaryDetailsTransformations;
