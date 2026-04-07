14b78333257199fe3ec0038e8797a11a6cb0f017
Ebadian <ali.ebadian@gmail.com>
2026-04-07 10:19:13 +0100

    adding my fix

--- runner/src/server/services/cacheService.ts
+++ runner/src/server/services/cacheService.ts
@@ -124,7 +124,8 @@ export class CacheService {
 
   async clearState(request: HapiRequest) {
     if (request.yar?.id) {
-      this.cache.drop(this.Key(request));
+      //this.cache.drop(this.Key(request));
+      request.yar.reset();
     }
   }
 
