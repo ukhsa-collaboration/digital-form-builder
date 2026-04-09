60e6ff3026fc63d3ef47e118db41e0235ab217e1
Micol Giannelli <micol.giannelli@accenture.com>
2026-04-09 15:30:02 +0100

    restored initilise session 2

--- runner/src/server/plugins/initialiseSession/initialiseSession.ts
+++ runner/src/server/plugins/initialiseSession/initialiseSession.ts
@@ -27,28 +27,6 @@ export const initialiseSession: Plugin<InitialiseSession> = {
   name: "initialiseSession",
   register: async function (server, options) {
     const { safelist } = options;
-
-    server.route({
-      method: "POST",
-      path: "/session/keep-alive",
-      options: {
-        auth: false,
-      },
-      handler: async (request, h) => {
-        const { cacheService } = request.services([]);
-
-        // Read existing state
-        const state = await cacheService.getState(request);
-
-        if (state) {
-          // Re-save without changes → refresh TTL
-          await cacheService.mergeState(request, {});
-        }
-
-        return h.response().code(204);
-      },
-    });
-
     server.route({
       method: "GET",
       path: "/session/{token}",
@@ -142,6 +120,27 @@ export const initialiseSession: Plugin<InitialiseSession> = {
         });
 
         return h.response({ token }).code(201);
+      },
+    });
+
+    server.route({
+      method: "POST",
+      path: "/session/keep-alive",
+      options: {
+        auth: false,
+      },
+      handler: async (request, h) => {
+        const { cacheService } = request.services([]);
+
+        // Read existing state
+        const state = await cacheService.getState(request);
+
+        if (state) {
+          // Re-save without changes → refresh TTL
+          await cacheService.mergeState(request, {});
+        }
+
+        return h.response().code(204);
       },
     });
   },
