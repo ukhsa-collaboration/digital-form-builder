07afdcf4d2012a59daab1611c9e6f99476e9879c
kitttang <katrina.tang@accenture.com>
2026-07-03 15:06:08 +0100

    remove srs changes from feature branch

--- runner/src/server/plugins/engine/components/Details.ts
+++ runner/src/server/plugins/engine/components/Details.ts
@@ -1,7 +1,5 @@
 import { FormData, FormSubmissionErrors } from "../types";
 import { ComponentBase } from "./ComponentBase";
-import config from "../../../config";
-import nunjucks from "nunjucks";
 
 export class Details extends ComponentBase {
   getViewModel(formData: FormData, errors: FormSubmissionErrors) {
@@ -12,10 +10,6 @@ export class Details extends ComponentBase {
       summaryHtml: this.title,
       html: this.content,
     };
-
-    if (config.allowUserTemplates) {
-      viewModel.html = nunjucks.renderString(viewModel.html, { ...formData });
-    }
 
     if ("condition" in options && options.condition) {
       viewModel.condition = options.condition;
