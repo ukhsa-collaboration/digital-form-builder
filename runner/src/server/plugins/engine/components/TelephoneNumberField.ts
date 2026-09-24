93d104eaeac9ea3df769a832d371f26f86e54145
kitttang <katrina.tang@accenture.com>
2026-09-24 01:40:42 +0100

    telephone number validation

--- runner/src/server/plugins/engine/components/TelephoneNumberField.ts
+++ runner/src/server/plugins/engine/components/TelephoneNumberField.ts
@@ -2,7 +2,12 @@ import { TelephoneNumberFieldComponent } from "@xgovformbuilder/model";
 
 import { FormComponent } from "./FormComponent";
 import { FormModel } from "../models";
-import { addClassOptionIfNone, internationalPhoneValidator } from "./helpers";
+import { addClassOptionIfNone } from "./helpers";
+import {
+  internationalPhoneValidator,
+  ukPhoneValidator,
+  ukAndInternationalValidator,
+} from "./telephoneHelpers";
 import { FormData, FormSubmissionErrors } from "../types";
 import joi, { Schema } from "joi";
 
@@ -33,20 +38,29 @@ export class TelephoneNumberField extends FormComponent {
 
     if (options.isInternational) {
       componentSchema = componentSchema.custom(internationalPhoneValidator);
-    }
-
-    if (options.customValidationMessages) {
-      componentSchema = componentSchema.messages(
-        options.customValidationMessages
-      );
+    } else if (options.isUK) {
+      componentSchema = componentSchema.custom(ukPhoneValidator);
     } else {
-      componentSchema = componentSchema.messages({
-        "string.pattern.base":
-          def.options?.customValidationMessage ?? DEFAULT_MESSAGE,
-      });
+      componentSchema = componentSchema.custom(ukAndInternationalValidator);
     }
-    this.schema = componentSchema;
 
+    componentSchema = componentSchema.messages({
+      "string.pattern.base": DEFAULT_MESSAGE,
+      "string.empty": DEFAULT_MESSAGE,
+      INVALID_COUNTRY_CODE: `${def.title} failed because invalid country calling code`,
+      NOT_A_NUMBER: `${def.title} failed because the string supplied did not seem to be a phone number`,
+      TOO_SHORT_AFTER_IDD: `${def.title} failed because phone number too short after IDD`,
+      TOO_SHORT_NSN: `${def.title} failed because the string supplied is too short to be a phone number`,
+      TOO_SHORT: `${def.title} failed because the string supplied is too short to be a phone number`,
+      TOO_LONG: `${def.title} failed because the string supplied is too long to be a phone number`,
+      IS_POSSIBLE_LOCAL_ONLY: DEFAULT_MESSAGE,
+      INVALID_LENGTH: DEFAULT_MESSAGE,
+      INVALID_NUMBER: DEFAULT_MESSAGE,
+      NON_UK_NUMBER: DEFAULT_MESSAGE,
+      ...(options.customValidationMessages || {}),
+    });
+
+    this.schema = componentSchema;
     addClassOptionIfNone(this.options, "govuk-input--width-20");
   }
 
